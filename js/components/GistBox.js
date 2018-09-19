4e23d44beef09bae479deca20039dfbd195ce560
hv <github.hv@gmail.com>
2018-09-19 15:43:52 +0800

    Version 1

---
+++ js/components/GistBox.js
@@ -0,0 +1,45 @@
+import Gist from './Gist';
+import GistAddForm from './GistAddForm';
+
+var GistBox = React.createClass({
+
+  getInitialState: function(){
+    return{
+      gists: []
+    };
+  },
+
+  addGist: function(username){
+    //Back ticks are used
+    var url = `https://api.github.com/users/${username}/gists`;
+
+    $.get(url, function(result){
+      var username = result[0].owner.login;
+      var url = result[0].html_url;
+
+      var gists = this.state.gists.concat({username, url});
+      this.setState({gists});
+
+    }.bind(this));
+  },
+
+  render: function(){
+
+      var newGist = function(gist){
+          return <Gist username={gist.username} url={gist.url}/>
+      };
+
+      return(
+        <div>
+          <h1>GistBox</h1>
+
+          <GistAddForm onAdd={this.addGist}/>
+          //{this.state.gists.map(gist => this.newGist(gist))}
+          {this.state.gists.map(newGist)}
+        </div>
+      );
+  }
+
+});
+
+export default GistBox;
